4c9560ef735c06fd67d2ca451166a7d710af768b
Marko A. Rodriguez <okrammarko@gmail.com>
2015-06-01 11:53:21 -0600

    added awaitIndexesOnline()

--- src/main/java/org/neo4j/tinkerpop/api/Neo4jGraphAPI.java
+++ src/main/java/org/neo4j/tinkerpop/api/Neo4jGraphAPI.java
@@ -6,9 +6,9 @@
  * the Apache License, Version 2.0 (the "License"); you may
  * not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p/>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p/>
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -20,29 +20,43 @@ package org.neo4j.tinkerpop.api;
 
 import java.util.Iterator;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public interface Neo4jGraphAPI {
 
-    Neo4jNode createNode(String...labels);
+    Neo4jNode createNode(String... labels);
 
     Neo4jNode getNodeById(long id);
+
     Neo4jRelationship getRelationshipById(long id);
 
     void shutdown();
+
     Iterable<Neo4jNode> allNodes();
+
     Iterable<Neo4jRelationship> allRelationships();
 
-    Iterable<Neo4jNode> findNodes(String property,Object value);
+    Iterable<Neo4jNode> findNodes(String property, Object value);
+
     Iterable<Neo4jNode> findNodes(String label);
+
     Iterable<Neo4jNode> findNodes(String label, String property, Object value);
+
+    void awaitIndexesOnline(final long time, final TimeUnit timeUnit);
+
     Neo4jTx tx();
 
-    Iterator<Map<String,Object>> execute(String query, Map<String,Object> params);
+    Iterator<Map<String, Object>> execute(String query, Map<String, Object> params);
 
     boolean hasSchemaIndex(String label, String property);
+
     Iterable<String> getKeys();
+
     Object getProperty(String key);
+
     boolean hasProperty(String key);
+
     Object removeProperty(String key);
+
     void setProperty(String key, Object value);
 }
